4b801c8a29858a949fcb4d1abd05b48c3acd600e
Zinmin Aung <zinminaung@Zinmins-MacBook-Air.local>
2026-05-05 20:31:15 +0700

    create login route controller and set cookies'

--- server/src/controllers/userController.ts
+++ server/src/controllers/userController.ts
@@ -35,7 +35,7 @@ const createUser = asyncHandler( async (req: Request, res: Response) => {
         // update password for unverified email (user)
         await pool.query("UPDATE users SET password_hash = $1 WHERE email = $2", [hashPassword, email])
         // create token for 5 min 
-        const token5min = jwt.sign({ id:  existUser.id}, process.env.JWT_KEY);
+        const token5min = jwt.sign({ id:  existUser.id}, process.env.JWT_KEY, { expiresIn: "5m" });
         await sendVerification(email, token5min);
         return res.status(200).json({message: "Password is Updated"})
         
@@ -44,13 +44,11 @@ const createUser = asyncHandler( async (req: Request, res: Response) => {
     const newUserQuery = await pool.query("INSERT INTO users (email, name, password_hash) VALUES ($1, $2, $3) RETURNING *", [email, name, hashPassword]);
     const newUser = newUserQuery.rows[0]
     // create token for 5 min 
-    const token5min = jwt.sign({ id:  newUser.id}, process.env.JWT_KEY);
+    const token5min = jwt.sign({ id:  newUser.id}, process.env.JWT_KEY, { expiresIn: "5m" });
     await sendVerification(email, token5min);
     return res.status(200).json({message: "User is Created"})
     
-
-    
-    
+  
 })
 
 // to verify email
@@ -76,5 +74,48 @@ const verifyEmail = asyncHandler( async (req: Request, res: Response) => {
 
 })
 
+const loginUser = asyncHandler( async (req: Request, res: Response) => {
+    const { email, password } = req.body || {};
+    
+    // check if user provide data
+    if (!email || !password)
+    {
+        return res.status(400).json({message: "Fill Required Data!"});
+    }
 
-module.exports = { createUser, verifyEmail }
+    // check if user already exist in database
+    const existingUserQuery = await pool.query("SELECT * FROM users WHERE email = $1", [email]);
+    if (existingUserQuery.rows.length < 1)
+    {
+        return res.status(404).json({message: "User not founded in database!"});
+    }
+
+    const existingUser = existingUserQuery.rows[0];
+    if (!existingUser.is_verified)
+    {
+        return res.status(404).json({message: "User not founded in database!"});   
+    }
+
+    // check password corect
+    const isPasswordMatch = await bcrypt.compare(password, existingUser.password_hash);
+
+    if (!isPasswordMatch)
+    {
+        return res.status(401).json({message: "Invalid credentials!"});
+    }
+
+    // create token 
+    const token10days = jwt.sign({ id:  existingUser.id}, process.env.JWT_KEY, { expiresIn: "10d" });
+
+    res.cookie("access_token", token10days, {
+        httpOnly: true,       // JS cannot access it
+        secure: process.env.NODE_ENV === "production",         // HTTPS only (use false in development)
+        sameSite: "strict",   // protects against CSRF
+        maxAge: 10 * 24 * 60 * 60 * 1000, // 10 days in milliseconds
+        });
+    return res.status(200).json({ message: "Login successful" });
+
+})
+
+
+module.exports = { createUser, verifyEmail, loginUser }
--- server/src/routes/userRoute.ts
+++ server/src/routes/userRoute.ts
@@ -1,11 +1,12 @@
 const express = require('express')
 const route = express.Router();
 
-const { createUser, verifyEmail } = require('../controllers/userController')
+const { createUser, verifyEmail, loginUser } = require('../controllers/userController')
 
 
 
 route.post('/create', createUser);
 route.get('/verify/:token', verifyEmail);
+route.post('/login', loginUser);
 
 module.exports = route
