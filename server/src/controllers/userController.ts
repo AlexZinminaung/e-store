e833734964b5de068203c038da497497f42d0370
Zinmin Aung <zinminaung@Zinmins-MacBook-Air.local>
2026-05-05 19:07:26 +0700

    add mailer for email verification

--- server/src/controllers/userController.ts
+++ server/src/controllers/userController.ts
@@ -2,6 +2,7 @@ import type {Request, Response} from 'express'
 const asyncHandler = require('express-async-handler')
 const bcrypt = require('bcrypt');
 const { pool } = require('../configs/database');
+const { sendVerification } = require('../utils/verifiler');
 
 // register a new user
 
@@ -16,7 +17,7 @@ const createUser = asyncHandler( async (req: Request, res: Response) => {
     }
 
     // check if passwor is long enough 
-    if (password.length) return res.status(400).json({message: "User password is must be 8 letter at least!"});
+    if (password.length < 8) return res.status(400).json({message: "User password is must be 8 letter at least!"});
     const hashPassword = await bcrypt.hash(password, 10);
 
     // check if user already exist in database
@@ -32,12 +33,13 @@ const createUser = asyncHandler( async (req: Request, res: Response) => {
 
         // update password for unverified email (user)
         await pool.query("UPDATE users SET password_hash = $1 WHERE email = $2", [hashPassword, email])
-
+        await sendVerification(email, 'token');
         return res.status(200).json({message: "Password is Updated"})
         
     }
     // create new user
     const newUserQuery = await pool.query("INSERT INTO users (email, name, password_hash) VALUES ($1, $2, $3)", [email, name, hashPassword]);
+    await sendVerification(email, 'token');
     return res.status(200).json({message: "User is Created"})
     
 
---
+++ server/src/utils/verifiler.ts
@@ -0,0 +1,34 @@
+const nodemailer = require("nodemailer");
+
+// Create a transporter using SMTP
+const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false, // use STARTTLS (upgrade connection to TLS after connecting)
+  auth: {
+    user: process.env.EMAIL,
+    pass: process.env.PSW,
+  },
+});
+
+const sendVerification = async (email: string, token: string) => {
+
+    try {
+        const info = await transporter.sendMail({
+            from: `"Email Verification For E-store" <${process.env.email}>`, // sender address
+            to: `${email}`, // list of recipients
+            subject: "Hello", // subject line
+            text: "Hello world?", // plain text body
+            html: "<b>Hello world?</b>", // HTML body
+        });
+
+        console.log("Message sent: %s", info.messageId);
+        // Preview URL is only available when using an Ethereal test account
+        console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+        } 
+        catch (err) {
+            console.error("Error while sending mail:", err);
+        }
+}
+
+module.exports = { sendVerification }
